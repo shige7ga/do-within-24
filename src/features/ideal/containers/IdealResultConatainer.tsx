e0f69b455b527908690bd813f851900dcd889323
shige7ga <ameft_80@yahoo.co.jp>
2026-06-12 23:00:45 +0900

    結果表示用ページで、データ取得方法をuseStateをしようせず、コンポーネントから直接取得へ変更

--- src/features/ideal/containers/IdealResultConatainer.tsx
+++ src/features/ideal/containers/IdealResultConatainer.tsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
 import { mockIdeal } from "../mocks/ideal";
-import type { Ideal } from "../../../types/ideal";
 import styles from "../../../styles/common.module.css";
 
 export default function IdealResultContainer() {
-  const [ideal, setIdeal] = useState<Ideal>(mockIdeal);
-
   return (
     <div>
       <h1 className={styles.title}>理想の叶える今日の一歩</h1>
       <div>
         <h3 className={styles.subtitle}>あなたの理想</h3>
-        <p className={styles.description}>{ideal.context}</p>
+        <p className={styles.description}>{mockIdeal.context}</p>
         <h3 className={styles.subtitle}>なぜ理想をかなえたいのか？</h3>
-        <p className={styles.description}>{ideal.benefits}</p>
+        <p className={styles.description}>{mockIdeal.benefits}</p>
         <h3 className={styles.subtitle}>理想を叶えると失うものは？</h3>
-        <p className={styles.description}>{ideal.drawbacks}</p>
+        <p className={styles.description}>{mockIdeal.drawbacks}</p>
         <h3 className={styles.subtitle}>24時間以内に実行することは？</h3>
-        <p className={styles.description}>{ideal.action}</p>
+        <p className={styles.description}>{mockIdeal.action}</p>
       </div>
     </div>
   );
