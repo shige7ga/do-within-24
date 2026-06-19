71dd54f491598f5a759322ad635523e697dacd02
shige7ga <ameft_80@yahoo.co.jp>
2026-06-19 20:42:10 +0900

    mockIdealをidealDataに命名変更

--- src/features/ideal/containers/IdealResultConatainer.tsx
+++ src/features/ideal/containers/IdealResultConatainer.tsx
@@ -1,4 +1,4 @@
-import { mockIdeal } from "../mocks/ideal";
+import { idealData } from "../data/ideal";
 import styles from "../../../styles/common.module.css";
 
 export default function IdealResultContainer() {
@@ -7,13 +7,13 @@ export default function IdealResultContainer() {
       <h1 className={styles.title}>理想に近づく今日の一歩</h1>
       <div>
         <h3 className={styles.subtitle}>あなたの理想</h3>
-        <p className={styles.description}>{mockIdeal.context}</p>
+        <p className={styles.description}>{idealData.context}</p>
         <h3 className={styles.subtitle}>なぜ理想をかなえたいのか？</h3>
-        <p className={styles.description}>{mockIdeal.benefits}</p>
+        <p className={styles.description}>{idealData.benefits}</p>
         <h3 className={styles.subtitle}>理想を叶えると失うものは？</h3>
-        <p className={styles.description}>{mockIdeal.drawbacks}</p>
+        <p className={styles.description}>{idealData.drawbacks}</p>
         <h3 className={styles.subtitle}>24時間以内に実行することは？</h3>
-        <p className={styles.description}>{mockIdeal.action}</p>
+        <p className={styles.description}>{idealData.action}</p>
       </div>
     </div>
   );
--- src/features/ideal/data/ideal.ts
+++ src/features/ideal/data/ideal.ts
@@ -1,6 +1,6 @@
 import type { Ideal } from "../../../types/ideal";
 
-export const mockIdeal: Ideal = {
+export const idealData: Ideal = {
     context: "海外旅行へ行く",
     benefits: "いろんな観光地に行き、初めての絶景をたくさん見る",
     drawbacks: "お金と時間がたくさんかかる",
